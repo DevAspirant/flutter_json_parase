f331dd8efb7a9f3d3cd2c8e015f5c3a1859b147e
ammarfalmban <devAspirant@gmail.com>
2019-03-02 19:17:43 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  // Module
+  List _data = await getJSON();
+  print(_data[0]["body"]);
+  for(var i=0;i<_data.length;i++){
+    print("Title: ${_data[i]["title"]}");
+  }
+
+  runApp(new MaterialApp(
+    home: new Scaffold(
+      appBar: AppBar(
+        title: new Text("Json parsin"),
+        backgroundColor: Colors.orange,
+      ),
+      body: new ListView.builder(
+        padding: const EdgeInsets.all(4.4),
+       itemCount: _data.length,
+        itemBuilder: (BuildContext context, int position){
+         return Column(
+           children: <Widget>[
+             Divider(height: 3.4,),
+             ListTile(
+               title: Text(_data[position]["title"],
+               style: TextStyle(
+                 fontSize: 17.2,
+                 fontWeight: FontWeight.bold
+               ),),
+               subtitle: Text(_data[position]["body"]),
+               leading: CircleAvatar(
+                 // radius: 4.6,
+                 backgroundColor: Colors.orange,
+                 child: Text(_data[position]["title"][0]),
+               ),
+               onTap: () => showTapMessage(context, _data[position]["title"]),
+             )
+           ],
+         );
+        }),
+    ),
+  ));
+}
+
+//
+void showTapMessage(BuildContext context, String message){
+  var alertDialog = new AlertDialog(
+    title: Text(message),
+    actions: <Widget>[
+      FlatButton(
+        onPressed: () => Navigator.of(context).pop(),
+  child: Text("ok"),
+      )
+    ],
+  );
+  showDialog(context: context, builder: (context){
+    return alertDialog;
+  });
+}
+
+// Abstaract class that get json data
+Future<List> getJSON() async {
+  String apiUrl = "https://jsonplaceholder.typicode.com/posts";
+  http.Response response = await http.get(apiUrl);
+  return json.decode(response.body);
+}
